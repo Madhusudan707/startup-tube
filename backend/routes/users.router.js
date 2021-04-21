e46279d0be598153c236a34ced5f76b7416a6ce8
Maddy@Xdev200 <xdev200200@gmail.com>
2021-04-21 11:14:05 +0530

    created:backend for signin & signup

--- backend/routes/users.router.js
+++ backend/routes/users.router.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const {Histories} =  require("../models/history.model")
+const {Users} =  require("../models/users.model")
 
 router.route("/")
 .get(async(req,res)=>{
     try{
-        const data = await Histories.find({})
+        const data = await Users.find({})
         res.json({success:true,data})
     }catch(err){
-        res.status(500).json({success:false,message:"Unable to load history",errorMessage:err.message})
+        res.status(500).json({success:false,message:"Unable to find user",errorMessage:err.message})
     }
 })
 
 .post(async (req,res)=>{
     try{
-        const histories = req.body;
-        const NewHistory = new Histories(histories)
-        const savedHistory = await NewHistory.save()
-        res.json({success:true,histories:savedHistory})
+        const users = req.body;
+        const NewUser = new Users(users)
+        const savedUser = await NewUser.save()
+        res.json({success:true,users:savedUser})
     }catch(err){
-        res.status(500).json({ success: false, message: "unable to add history", errorMessage: err.message})
+        res.status(500).json({ success: false, message: "unable to register", errorMessage: err.message})
     }
 })
 module.exports = router
