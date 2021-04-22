602e762f62c33e5fd760eb46b2d118c217166d89
Maddy@Xdev200 <xdev200200@gmail.com>
2021-04-22 14:31:49 +0530

    Added: Login with Facebook

--- src/components/Header/Login/Login.jsx
+++ src/components/Header/Login/Login.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React,{useState} from "react";
+import FacebookLogin from 'react-facebook-login';
 import styles from "../../../styles/login.module.css";
 import { Button } from "../../Button/Button";
 import '../../../styles/social_logo.css'
 import {useTheme} from '../../../contexts/index'
-import {useSignin} from '../../../hooks/index'
+import {useSignin,useOtp} from '../../../hooks/index'
 export const Login = () => {
+
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState('');
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  }
+
+
   const{theme} = useTheme()
   const {signInHandler,username,password} = useSignin()
+  const {otpHandler,otp} = useOtp()
   return (
     <div className={styles.login}>
       <a href="#loginModal">
@@ -21,7 +40,7 @@ export const Login = () => {
               {/* <span><i className="fas fa-lock fa-3x"></i></span> */}
             <input type="text" placeholder="Enter Username" ref={username} />
             <input type="password" placeholder="Enter Password" ref={password} />
-            <Button btnText="SUBMIT" func={signInHandler}/>
+            <a href='#otpModal'><Button btnText="SUBMIT" func={signInHandler}/></a>
             <span>
               <label htmlFor="remember_me">
                <input type="checkbox" id="remember_me" />
@@ -34,18 +53,39 @@ export const Login = () => {
             <div className={styles.social_login}>
               <h2>Social Login</h2>
               <span>
-               <a href="#!"> <i
+               {/* <a href="#!"> <i
                   className="fas fa-mobile-alt fa-3x"
                   title="Login With Mobile"
-                ></i></a>
-                <a href="#!"> <i className="fab fa-facebook-square fa-3x fb"></i></a>
-                <a href="#!"> <i className="fab fa-twitter-square fa-3x twitter"></i></a>
-                <a href="#!"><i className="fab fa-google fa-3x google"></i></a>
+                ></i></a> */}
+                { !login && 
+            <FacebookLogin
+              appId="562118384400275"
+              autoLoad={true}
+              fields="name,email,picture"
+              scope="public_profile,user_friends"
+              callback={responseFacebook}
+              icon="fa-facebook" />
+          }
+                {/* <a href="#!"> <i className="fab fa-twitter-square fa-3x twitter"></i></a>
+                <a href="#!"><i className="fab fa-google fa-3x google"></i></a> */}
               </span>
               <a href='#registerModal'>Not registered? Create an account</a>
             </div>
           </div>
         </div>
+      </div>
+      <div id="otpModal" className={styles.modal} autoFocus={false}>
+      <div className={`${styles.modal_content_otp} ${theme}`}>
+          <div className={styles.otpInput}>
+            <input type='number' ref={otp} placeholder="Enter OTP here"/>
+          </div>
+          <div className={styles.otp_actions}>
+          <Button btnText='SUBMIT' func={otpHandler}/>
+          <Button btnText='RESEND OTP'/>
+           
+           
+          </div>
+          </div>
       </div>
     </div>
   );
