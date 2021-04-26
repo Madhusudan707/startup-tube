ad1a47f67b0f390fb319a4a9306f7ab723703120
Maddy@Xdev200 <xdev200200@gmail.com>
2021-04-26 11:17:42 +0530

    fix: json token error

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import {Header,Footer,StyleSwitcher} from './components/index'
+import { Header, Footer, StyleSwitcher } from "./components/index";
 import { Routes, Route } from "react-router-dom";
 import routes from "./routing/routes";
-import './styles/App.css';
-import { useEffect } from 'react';
-import {useTheme} from './contexts/index'
-import {usePathName} from './hooks/index'
-import {useUser} from './contexts'
+import "./styles/App.css";
+import { useEffect } from "react";
+import { useTheme } from "./contexts/index";
+import { usePathName } from "./hooks/index";
+import { useUser } from "./contexts";
 
-
-const App=()=> {
-  const {setOpacity} = useTheme()
-  const {pathUrl} = usePathName()
-  const {login,setLogin,image,setImage,name,setName} = useUser()
-  useEffect(()=>{
-      pathUrl==="/player"?setOpacity("opacity_light"):setOpacity("opacity_default")
-       //Below Line remove the useEffect dependency warning
+const App = () => {
+  const { setOpacity } = useTheme();
+  const { pathUrl } = usePathName();
+  const { login, setLogin, image, setImage, name, setName } = useUser();
+  useEffect(() => {
+    pathUrl === "/player"
+      ? setOpacity("opacity_light")
+      : setOpacity("opacity_default");
+    //Below Line remove the useEffect dependency warning
     //eslint-disable-next-line react-hooks/exhaustive-deps
-  },[pathUrl])
+  }, [pathUrl]);
 
   useEffect(() => {
     (async () => {
       const login = await JSON.parse(localStorage.getItem("login"));
-      const image = await JSON.parse(localStorage.getItem("image"))
-      const name = await JSON.parse(localStorage.getItem("name"))
+      const image = await JSON.parse(localStorage.getItem("image"));
+      const name = await JSON.parse(localStorage.getItem("name"));
       if (login) {
         setLogin(login);
         setImage(image);
@@ -41,10 +42,9 @@ const App=()=> {
 
   return (
     <div className="App">
-      
-      <Header/>
-      <StyleSwitcher/>
-      <Footer/>
+      <Header />
+      <StyleSwitcher />
+      <Footer />
       <Routes>
         {routes.map((route, key) => (
           <Route path={route.path} element={<route.component />} key={key} />
@@ -52,6 +52,6 @@ const App=()=> {
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
