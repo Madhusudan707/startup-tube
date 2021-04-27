a4966ae22f77c061591acd93ed3a2f6752224b46
Maddy@Xdev200 <xdev200200@gmail.com>
2021-04-27 12:27:27 +0530

    testing: fb login

--- src/hooks/useFacebook.js
+++ src/hooks/useFacebook.js
@@ -17,7 +17,7 @@ export const useFacebook = () => {
   const responseFacebook = async (response) => {
     
     if (response.id === await localStorage.getItem("fbid")) {
-      console.log("1")
+     
       setImage(response.picture.data.url);
       setName(response.name);
       setFbid(response.id);
@@ -27,8 +27,24 @@ export const useFacebook = () => {
       await localStorage.setItem("image", response.picture.data.url);
       setLogin(true);
       navigate("user_profile");
+    }else if(response.id){
+      try{
+          const response = await axios.post(`https://startup-tube-backend.herokuapp.com/users/fb/${response.id}`)
+      
+          setImage(response.picture.data.url);
+          setName(response.name);
+          setFbid(response.id);
+          await localStorage.setItem("login", true);
+          await localStorage.setItem("fbid", response.id);
+          await localStorage.setItem("name", response.name);
+          await localStorage.setItem("image", response.picture.data.url);
+          setLogin(true);
+          navigate("user_profile");
+        }catch(err){
+        console.log(`${err}:Unable to find user using fb id`)
+      }
     } else if (response.accessToken) {
-      console.log("1")
+     
       try {
         await axios.post("https://startup-tube-backend.herokuapp.com/users", {
           name: response.name,
