7fdc5490e21df816a49fbf6d99d58e392d17b1a4
Maddy@Xdev200 <xdev200200@gmail.com>
2021-04-28 22:20:54 +0530

    update: brand icon

--- src/components/Header/Brand/Brand.jsx
+++ src/components/Header/Brand/Brand.jsx
@@ -2,7 +2,7 @@ import styles from '../../../styles/brand.module.css'
 export const Brand = () => {
     return (
         <div className={styles.brand}>
-             <span><i className="fas fa-cog fa-spin fa-3x  fa-spin"></i></span>
+             <span><img src='images/rocket.svg' alt='brand'/></span>
             <h2 className={styles.typing}>Startup-Tube</h2>
         </div>
     )
