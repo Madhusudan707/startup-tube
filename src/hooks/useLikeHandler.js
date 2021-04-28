bbc2784eef9bee526ca1a18e664a19509bd7185a
Maddy@Xdev200 <xdev200200@gmail.com>
2021-04-28 19:50:59 +0530

    update: api endpoints

--- src/hooks/useLikeHandler.js
+++ src/hooks/useLikeHandler.js
@@ -1,24 +1,17 @@
 import { useState } from "react";
 import { useVideo, useLike, useVideoId } from "../contexts/index";
-
+import axios from "axios";
 export const useLikeHandler = () => {
   const { likeDispatch } = useLike();
   const { videoState, videoDispatch } = useVideo();
   const { vid } = useVideoId();
   const [isLike, setIsLike] = useState(false);
-  const [isDisLike, setDisLike] = useState(false);
 
-  const likeHandler = () => {
+  const likeHandler = async () => {
+    const userId = await localStorage.getItem("_id");
     const newArr = videoState.data.map((video) => {
-      if (video.id === vid) {
-        if (isDisLike) {
-          setIsLike(false);
-          setDisLike(true);
-        } else {
-          setIsLike(true);
-          setDisLike(false);
-        }
-
+      if (video._id === vid) {
+        setIsLike(!isLike);
         return { ...video, favorite: !video.favorite };
       }
       return video;
@@ -32,21 +25,57 @@ export const useLikeHandler = () => {
       type: "ADDED_LIKE_VIDEO",
       payload: { likeVideo: likeArray },
     });
-  };
 
-  const dislikeHandler = () => {
-    if (isLike) {
-      setIsLike(false);
-      setDisLike(true);
-    } else {
-      setIsLike(true);
-      setDisLike(false);
+    try {
+      const response = await axios.get(
+        `https://startup-tube-backend.herokuapp.com/users-activity/user/${userId}`
+      );
+
+      if (response.data.data) {
+        try {
+          const response = await axios.get(
+            `https://startup-tube-backend.herokuapp.com/users-activity/user/${userId}/like/${vid}`
+          );
+          console.log(response.data.success);
+          if (!response.data.success) {
+            try {
+              await axios.post(
+                `https://startup-tube-backend.herokuapp.com/users-activity/user/like/update/${userId}`,
+                {
+                  like: vid,
+                }
+              );
+            } catch (err) {
+              console.log(`${err}: Unable to Save the Like, Try again`);
+            }
+          } else {
+            //Remove videoId if it is present
+            try {
+              await axios.post(
+                `https://startup-tube-backend.herokuapp.com/users-activity/user/like/remove/${userId}`,
+                {
+                  like:vid
+                }
+              );
+            } catch (err) {}
+          }
+        } catch (err) {}
+      } else {
+        try {
+          await axios.post(`https://startup-tube-backend.herokuapp.com/users-activity`, {
+            uid: userId,
+            like: vid,
+          });
+        } catch (err) {
+          console.log(`${err}: Unable to Save the Like and userID, Try again`);
+        }
+      }
+    } catch (err) {
+      console.log(err, "Unable to Find the user, Try Again");
     }
   };
   return {
     likeHandler,
-    dislikeHandler,
-    isDisLike,
     likeDispatch,
     vid,
     isLike,
--- src/hooks/usePostHistory.js
+++ src/hooks/usePostHistory.js
@@ -7,10 +7,40 @@ export const usePostHistory = () => {
     const userId = await localStorage.getItem("_id");
     setVid(videoId);
     try {
-      await axios.post("https://startup-tube-backend.herokuapp.com/histories", {
-        uid: userId,
-        vid: videoId,
-      });
+      const response = await axios.get(
+        `https://startup-tube-backend.herokuapp.com/users-activity/user/${userId}`
+      );
+
+      if (response.data.data) {
+        try {
+          const response = await axios.get(
+            `https://startup-tube-backend.herokuapp.com/users-activity/user/${userId}/history/${videoId}`
+          );
+          if (!response.data.success) {
+            try {
+              await axios.post(
+                `https://startup-tube-backend.herokuapp.com/users-activity/user/history/update/${userId}`,
+                {
+                  history: videoId,
+                }
+              );
+            } catch (err) {
+              console.log(`${err}: Unable to Save the History, Try again`);
+            }
+          }
+        } catch (err) {}
+      } else {
+        try {
+          await axios.post(`https://startup-tube-backend.herokuapp.com/users-activity`, {
+            uid: userId,
+            history: videoId,
+          });
+        } catch (err) {
+          console.log(
+            `${err}: Unable to Save the History and userID, Try again`
+          );
+        }
+      }
     } catch (err) {
       console.log(err, "Unable to Save History, Try Again");
     }
